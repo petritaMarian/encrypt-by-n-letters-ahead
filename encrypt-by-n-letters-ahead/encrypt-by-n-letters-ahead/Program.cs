789a00647bb0e61be3ed3bfa14ecd1d1b1aac8c1
Petrita Marian <146872240+petritaMarian@users.noreply.github.com>
2024-10-14 16:12:20 +0300

    Add files via upload

---
+++ encrypt-by-n-letters-ahead/encrypt-by-n-letters-ahead/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace encrypt_by_n_letters_ahead
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Provide input to be encrypted: " + "\n");
+            string input = Console.ReadLine();
+
+            Console.WriteLine("By how many letters ahead? [1; 25]");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.Write("Encrypted input: ");
+
+            string[] words = input.Split(' ');
+
+            foreach (string word in words)
+            {
+                for (int i = 0; i < word.Length; i += 1)
+                {
+                    char character = word[i];
+                    if (char.IsLetter(character))
+                    {
+                        if (character >= 'A' && character <= 'Z')
+                            if ((char)(character + n) > 'Z')
+                                character = (char)(n - ('Z' - character) + 'A' - 1);
+                            else character = (char)(character + n);
+
+                        else if (character >= 'a' && character <= 'z')
+                            if ((char)(character + n) > 'z')
+                                character = (char)(n - ('z' - character) + 'z' - 1);
+                            else character = (char)(character + n);
+
+                        Console.Write(character);
+                    }
+                    else Console.Write(character);
+                }
+                Console.Write(" ");
+            }
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ encrypt-by-n-letters-ahead/encrypt-by-n-letters-ahead/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("encrypt-by-n-letters-ahead")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("encrypt-by-n-letters-ahead")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a99e1950-630d-49d3-930e-4fa3c167d745")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
